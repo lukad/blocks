1d02b8e88d332629b0e55a5c9e4e6dd6fe9e3d3b
Luka Dornhecker <luka.dornhecker@hey.com>
2022-05-02 20:44:14 +0200

    use bevy_flycam

--- src/main.rs
+++ src/main.rs
@@ -5,34 +5,7 @@ use voxel_world::VoxelPlugin;
 use bevy::{
     diagnostic::{FrameTimeDiagnosticsPlugin, LogDiagnosticsPlugin},
     prelude::*,
-    render::camera::PerspectiveProjection,
 };
-
-fn setup(mut commands: Commands) {
-    commands.spawn_bundle(PerspectiveCameraBundle {
-        transform: Transform::from_translation(Vec3::new(0.0, 0.0, 10.0))
-            .looking_at(Vec3::new(0.0, 0.0, 0.0), Vec3::Y),
-        perspective_projection: PerspectiveProjection {
-            near: 0.001,
-            far: 1000.0,
-            ..Default::default()
-        },
-        ..Default::default()
-    });
-    // .insert(FlyCamera {
-    //     sensitivity: 25.0,
-    //     ..Default::default()
-    // });
-}
-
-fn cursor_grab_system(mut windows: ResMut<Windows>, btn: Res<Input<MouseButton>>) {
-    let window = windows.get_primary_mut().unwrap();
-
-    if btn.just_pressed(MouseButton::Left) {
-        window.set_cursor_lock_mode(true);
-        window.set_cursor_visibility(false);
-    }
-}
 
 fn main() {
     App::new()
@@ -41,12 +14,10 @@ fn main() {
             ..Default::default()
         })
         .add_plugins(DefaultPlugins)
+        .add_plugin(bevy_flycam::PlayerPlugin)
         .add_plugin(FrameTimeDiagnosticsPlugin::default())
         .add_plugin(LogDiagnosticsPlugin::default())
         .add_plugin(VoxelPlugin)
-        // .add_plugin(FlyCameraPlugin)
-        .add_startup_system(setup)
         .add_system(bevy::input::system::exit_on_esc_system)
-        .add_system(cursor_grab_system)
         .run();
 }
